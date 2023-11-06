601878817f45050ecb6b0c0953119794eeb235f9
HoaxW <58890975+HoaxW@users.noreply.github.com>
2023-11-06 21:03:26 +0100

    SeedPouch

--- Assets/Scripts/FootStepsAudio.cs
+++ Assets/Scripts/FootStepsAudio.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class FootStepsAudio : MonoBehaviour
 {
     public AudioClip[] steps;
-    public List<AudioClip> randomlist;
+    private List<AudioClip> randomlist;
     AudioSource source;
 
     [SerializeField] float stepSpeed = 0.5f;
